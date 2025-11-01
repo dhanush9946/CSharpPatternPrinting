ca89ff96fd7b6a8df9e66547a4f4db87917a9472
dhanush9946 <dhanushpoothanganam5@gmail.com>
2025-11-01 12:53:16 +0530

    star patter1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿public class Pattern
+{
+    public static void Main()
+    {
+        int row, col, num;
+
+        Console.WriteLine("Enter the lengthh of the row");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        for (row = 0; row < num; row++)
+        {
+            for (col = 0; col <= row; col++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadLine();
+    }
+}
